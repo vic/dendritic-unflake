947bf6e7e6d92696d226211df698c686ed317d74
Victor Borja <vborja@apache.org>
2025-12-11 23:58:01 -0600

    update falake, use flakeModules.modules

--- modules/options/modules.nix
+++ modules/options/modules.nix
@@ -1,8 +1,4 @@
-{ lib, ... }:
+{ pins, ... }:
 {
-  options.flake.modules = lib.mkOption {
-    description = "<class>.<aspect> modules. akin to flake-parts' flake.modules";
-    default = { };
-    type = lib.types.lazyAttrsOf (lib.types.lazyAttrsOf lib.types.deferredModule);
-  };
+  imports = [ (import pins.falake).flakeModules.modules ];
 }
